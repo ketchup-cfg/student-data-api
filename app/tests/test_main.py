e9089c0acd17a7d792cea2b2136c18887e7e1936
Trevor Pierce <yeahtrevorpierce@gmail.com>
2023-01-10 20:38:27 -0500

    Simplified test for /students route

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -6,10 +6,6 @@ from app.settings import settings
 client = TestClient(app)
 
 
-def test_get_students_returns_list_of_students():
+def test_get_students_returns_200_status_code():
     response = client.get(f"{settings.API_V1_STR}/students")
     assert response.status_code == 200
-    assert response.json() == [
-        {"name": "Trevor Pierce"},
-        {"name": "Not Trevor Pierce"},
-    ]
